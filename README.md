e10b0d43558518d602d6c3f6920b41d7285194f5
David González <braben@gmail.com>
2019-01-05 10:15:45 +0000

    Initial shameless copy

---
+++ README.md
@@ -0,0 +1,10 @@
+Demo for [redux-form-auto](https://github.com/dgonz64/redux-form-auto) with bootstrap4.
+
+## [Play with the demo](https://dgonz64.github.io/redux-form-auto/demo.html)
+
+## Build
+
+    $ npm install
+    $ npm run build
+
+Then find your webs in `docs/index.html`
---
+++ src/components/App.jsx
@@ -0,0 +1,106 @@
+import React, { Component } from 'react'
+import brace from 'brace'
+import AceEditor from 'react-ace'
+
+import 'brace/mode/javascript'
+import 'brace/mode/jsx'
+import 'brace/theme/github'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import './app.css'
+
+import { compose } from 'redux'
+import { connect } from 'react-redux'
+import { editing } from '../ducks'
+
+import { DemoForm } from './DemoForm'
+import { DemoConfig } from './DemoConfig'
+import { formCode } from './formCode'
+import { Panel } from 'redux-form-auto-bootstrap4'
+import { formValues } from 'redux-form'
+
+const handleChange = (changeCode, code) => {
+  changeCode(code)
+}
+
+const handleConfigChange = (changeConfig, config) => {
+  changeConfig(config)
+}
+
+class AppBase extends Component {
+  componentDidMount() {
+    const { editor } = this.sampleElement
+
+    editor.setOptions({
+      readOnly: true,
+      highlightActiveLine: false,
+      highlightGutterLine: false
+    })
+
+    editor.renderer.$cursorLayer.element.style.display = 'none'
+  }
+
+  render() {
+    const {
+      code,
+      changeCode,
+      changeConfig,
+      config
+    } = this.props
+
+    return (
+      <div className="row app">
+        <h2 className="demo-title">
+          <a href="https://github.com/dgonz64/redux-form-auto">
+            redux-form-auto-bootstrap4 demo
+          </a>
+        </h2>
+        <div className="col-md-5 code">
+          <Panel header="Schema (edit and see the results in real time)">
+            <AceEditor
+              mode="javascript"
+              theme="github"
+              onChange={handleChange.bind(null, changeCode)}
+              value={code}
+              width="90%"
+              name="editor"
+            />
+            <p><small>
+                Editor thanks to{' '}
+                <a href="https://github.com/securingsincity/react-ace">
+                  React-Ace
+                </a>
+            </small></p>
+          </Panel>
+        </div>
+        <div className="col-md-5 form">
+          <Panel header="Form parameters">
+            <DemoConfig
+              onChange={handleConfigChange.bind(null, changeConfig)}
+              config={config}
+            />
+          </Panel>
+          <Panel header="Form element">
+            <AceEditor
+              mode="jsx"
+              theme="github"
+              value={formCode(config)}
+              name="sample"
+              maxLines={5}
+              readOnly
+              ref={element => this.sampleElement = element} 
+            />
+          </Panel>
+          <Panel header="Generated form" panelType="primary">
+            <DemoForm code={code} config={config} />
+          </Panel>
+        </div>
+      </div>
+    )
+  }
+}
+
+export const App = connect(
+  editing.mapState,
+  editing.mapActions
+)(AppBase)
